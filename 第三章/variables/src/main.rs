8d4170ef09779bdc9cb42fa31f155dea3b6871bf
wulongjiang <562445380@qq.com>
2022-12-01 09:46:00 +0800

    '更新文件结构'

---
+++ 第三章/variables/src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * @Author: wlj
+ * @Date: 2022-12-01 09:43:57
+ * @LastEditors: wlj
+ * @LastEditTime: 2022-12-01 09:44:20
+ * @Description: 变量和可变性
+ * @see:https://kaisery.github.io/trpl-zh-cn/ch03-01-variables-and-mutability.html
+ */
+fn main() {
+    println!("Hello, world!");
+}
