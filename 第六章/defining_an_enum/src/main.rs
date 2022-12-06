bbfe8c0854984e4e6e66d1b72de88fede1fc6076
wulongjiang <562445380@qq.com>
2022-12-06 17:32:38 +0800

    '第六章6.1枚举的定义学习'

---
+++ 第六章/defining_an_enum/src/main.rs
@@ -0,0 +1,13 @@
+/*
+ * @Author: wlj
+ * @Date: 2022-12-06 15:48:27
+ * @LastEditors: wlj
+ * @LastEditTime: 2022-12-06 17:09:13
+ * @Description: 枚举的定义
+ * @see:https://kaisery.github.io/trpl-zh-cn/ch06-01-defining-an-enum.html
+ */
+
+//结构体给予你将字段和数据聚合在一起的方法，像Rectangle
+fn main() {
+    println!("Hello, world!");
+}
